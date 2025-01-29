f11cacc67de9494c42560665bce6ef082502e59f
Aniket Kumar <aniketpundir348@gmail.com>
2025-01-29 17:59:20 +0530

    Restaurent admin portal and login portal

--- Backend/controller/restroController.js
+++ Backend/controller/restroController.js
@@ -1,97 +1,103 @@
-import bcrypt from 'bcrypt';
-import restroModel from '../models/restroModel.js';
+import bcrypt from "bcrypt";
+import restroModel from "../models/restroModel.js";
 import jwt from "jsonwebtoken";
-import otpModel from '../models/otpModel.js';
+import otpModel from "../models/otpModel.js";
 import fs from "fs";
 
 const createToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET)
-}
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
 
 const restroRegister = async (req, res) => {
-    try {
+  try {
+    let image = `${req.file.filename}`;
+    console.log(image);
+    const {
+      email,
+      password,
+      phone,
+      name,
+      locality,
+      district,
+      state,
+      pin_code,
+      lati,
+      longi,
+      otp,
+    } = req.body;
 
-        let image = `${req.file.filename}`
-        const { email, password, phone, name, locality, district, state, pin_code, lati, longi, otp } = req.body;
+    const v_otp = await otpModel.find({ email }).sort({ Date: -1 });
 
-        const v_otp = await otpModel.find({ email }).sort({ Date: -1 });
+    const isVaild = (v_otp) => {
+      const current = Date.now();
+      const tenmin = 10 * 60 * 1000;
+      const otpDate = v_otp[0].Date;
+      return tenmin >= current - otpDate;
+    };
 
+    if (otp == v_otp[0].otp && isVaild(v_otp)) {
+      await otpModel.deleteMany({ email });
+      const exists = await restroModel.findOne({ email: email });
+      if (exists) {
+        return res.json({
+          success: false,
+          message: "restaurent already exists",
+        });
+      } else if (password.lenth < 8) {
+        return res.json({ success: false, message: "password is too weak" });
+      } else {
+        const salt = await bcrypt.genSalt(10);
+        const hashedpassword = await bcrypt.hash(password, salt);
 
-        const isVaild = (v_otp) => {
-            const current = Date.now();
-            const tenmin = 10 * 60 * 1000;
-            const otpDate = v_otp[0].Date;
-            return tenmin >= current - otpDate;
+        const obj = {
+          locality: locality,
+          district: district,
+          state: state,
+          pin_code: pin_code,
+          lati: lati,
+          longi: longi,
+        };
 
-        }
+        const restro = new restroModel({
+          name: name,
+          address: obj,
+          image: image,
+          email: email,
+          password: hashedpassword,
+          phone: phone,
+        });
 
-        if (otp == v_otp[0].otp && isVaild(v_otp)) {
-            await otpModel.deleteMany({ email });
-            const exists = await restroModel.findOne({ email: email });
-            if (exists) {
-                return res.json({ success: false, message: "restaurent already exists" });
-            }
-            else if (password.lenth < 8) {
-                return res.json({ success: false, message: "password is too weak" });
-            }
-            else {
-
-                const salt = await bcrypt.genSalt(10);
-                const hashedpassword = await bcrypt.hash(password, salt)
-
-                const obj = {
-                    locality: locality,
-                    district: district,
-                    state: state,
-                    pin_code: pin_code,
-                    lati: lati,
-                    longi: longi
-                }
-
-                const restro = new restroModel({
-                    name: name,
-                    address: obj,
-                    image: image,
-                    email: email,
-                    password: hashedpassword,
-                    phone: phone,
-                })
-
-
-                await restro.save();
-                res.json({ success: true, message: 'application submitted successfully' });
-            }
-
-        }
-        else {
-            fs.unlink(`restro_uploads/${image}`,()=>{});
-            res.json({ success: false, message: "otp is incorrect or expired" });
-        }
-
-
-
+        await restro.save();
+        res.json({
+          success: true,
+          message: "application submitted successfully",
+        });
+      }
+    } else {
+      fs.unlink(`restro_uploads/${image}`, () => {});
+      res.json({ success: false, message: "otp is incorrect or expired" });
     }
-    catch (e) {
-        console.log(e);
-        res.json({ success: false, message: "error" })
-    }
-}
+  } catch (e) {
+    console.log(e);
+    res.json({ success: false, message: "error" });
+  }
+};
 
 const restroLogin = async (req, res) => {
-    try {
-        const { password, email } = req.body;
-        const restro = await restroModel.findOne({ email: email })
-        const compare = bcrypt.compare(password, restro.password);
-        if (compare) {
-            const token = createToken(restro._id);
-            res.json({ success: true, message: "login successfull", token });
-        } else {
-            res.json({ success: false, message: "invaild credentials" });
-        }
-    } catch (e) {
-        console.log(e)
-        res.json({ success: false, message: "Login failed" });
+  try {
+    const { password, email } = req.body;
+    const restro = await restroModel.findOne({ email: email });
+    const compare = bcrypt.compare(password, restro.password);
+    if (compare) {
+      const token = createToken(restro._id);
+      res.json({ success: true, message: "login successfull", token });
+    } else {
+      res.json({ success: false, message: "invaild credentials" });
     }
-}
+  } catch (e) {
+    console.log(e);
+    res.json({ success: false, message: "Login failed" });
+  }
+};
 
 export { restroRegister, restroLogin };
