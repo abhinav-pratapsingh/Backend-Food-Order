b01a7dc8e9e300c953b25950bcc8c3e31520c265
Aniket Kumar <aniketpundir348@gmail.com>
2025-01-31 22:11:09 +0530

    Api problem solve

--- Backend/controller/mapController.js
+++ Backend/controller/mapController.js
@@ -3,42 +3,42 @@ import restroModel from "../models/restroModel.js";
 import fetch from "node-fetch";
 
 const api_key = process.env.apiKey;
-const url = 'https://api.openrouteservice.org/v2/matrix/driving-car';
+const url = "https://api.openrouteservice.org/v2/matrix/driving-car";
 
 const calculateDistance = async (origin, destination) => {
-    const requestBody = {
-        locations: [origin, ...destination.map((i) => i.coordinates)], // Add origin first, followed by destinations
-        metrics: ["distance", "duration"],   // Get distance and duration
-        sources: [0]     // Index of the origin (first coordinate)
-    };
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Authorization': api_key,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(requestBody),
-        });
+  const requestBody = {
+    locations: [origin, ...destination.map((i) => i.coordinates)], // Add origin first, followed by destinations
+    metrics: ["distance", "duration"], // Get distance and duration
+    sources: [0], // Index of the origin (first coordinate)
+  };
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: api_key,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    });
 
-        const data = await response.json();
+    const data = await response.json();
 
-        // Distances from origin to destinations
-        const proceededData = data.distances[0].slice(1).filter((v)=> v <= 11000).map((itr, index) => (
-            
-            {
-                name: destination[index].name,
-                _id: destination[index]._id,
-                address:destination[index].address,
-                image:destination[index].image,
-                distance: Math.floor(itr / 100) / 10
-            }))
-        return (proceededData);
-    }
-    catch (error) {
-        console.error('Error calculating distance:', error);
-        return null;
-    }
+    // Distances from origin to destinations
+    const proceededData = data.distances[0]
+      .slice(1)
+      .filter((v) => v <= 11000)
+      .map((itr, index) => ({
+        name: destination[index].name,
+        _id: destination[index]._id,
+        address: destination[index].address,
+        image: destination[index].image,
+        distance: Math.floor(itr / 100) / 10,
+      }));
+    return proceededData;
+  } catch (error) {
+    console.error("Error calculating distance:", error);
+    return null;
+  }
 };
 
 // const nearRestro = async (req, res) => {
@@ -65,35 +65,34 @@ const calculateDistance = async (origin, destination) => {
 
 // }
 
-
 const nearRestro = async (req, res) => {
-    try{
-
+  try {
     const origin = [req.body.longi, req.body.lati];
     const district = req.body.district;
-    const restros = await restroModel.find({ 'address.district': district, status: 3 });
-    if(restros.length === 0){
-        console.log(restros)
-        res.json({success:false,message:"no restro found"})
-    }
-    else{
-    const coordinatesArray = await restros.map((restros) => ({
+    console.log(req.headers);
+    const restros = await restroModel.find({
+      "address.district": district,
+      status: 3,
+    });
+    if (restros.length === 0) {
+      console.log(restros);
+      res.json({ success: false, message: "no restro found" });
+    } else {
+      const coordinatesArray = await restros.map((restros) => ({
         coordinates: [restros.address.longi, restros.address.lati],
         name: restros.name,
         _id: restros._id,
-        address:restros.address,
-        image:restros.image
+        address: restros.address,
+        image: restros.image,
+      }));
+      const data = await calculateDistance(origin, coordinatesArray);
+      const dataSorted = data.sort((a, b) => b.distance - a.distance).reverse();
 
-    }))
-    const data = await calculateDistance(origin, coordinatesArray);
-    const dataSorted = data.sort((a, b) => b.distance - a.distance).reverse();
-
-    res.json({ success: true, data: dataSorted });}
-}catch{
-    res.json({success:false,message:"something went wrong"})
-}
-
-}
-
+      res.json({ success: true, data: dataSorted });
+    }
+  } catch {
+    res.json({ success: false, message: "something went wrong" });
+  }
+};
 
 export { nearRestro };
--- Backend/routes/restroRoutes.js
+++ Backend/routes/restroRoutes.js
@@ -1,15 +1,20 @@
-import express from 'express';
-import { districtRestro, restroLogin,restroRegister } from '../controller/restroController.js';
-import { authMiddleware, checkRestroStatus } from '../middleware/auth.js';
-import { nearRestro } from '../controller/mapController.js';
-import trimValues from '../middleware/trimValues.js';
-import upload_middleware from '../config/multer.js';
+import express from "express";
+import {
+  districtRestro,
+  restroLogin,
+  restroRegister,
+} from "../controller/restroController.js";
+import { authMiddleware, checkRestroStatus } from "../middleware/auth.js";
+import { nearRestro } from "../controller/mapController.js";
+import trimValues from "../middleware/trimValues.js";
+import upload_middleware from "../config/multer.js";
+import multer from "multer";
 
 const restroRouter = express.Router();
 
 restroRouter.post("/register", upload_middleware, trimValues, restroRegister);
-restroRouter.post("/login",trimValues,checkRestroStatus,restroLogin);
-restroRouter.post("/near",trimValues,nearRestro);
-restroRouter.post("/district",trimValues,districtRestro);
+restroRouter.post("/login", trimValues, checkRestroStatus, restroLogin);
+restroRouter.post("/near", multer().none(), trimValues, nearRestro);
+restroRouter.post("/district", trimValues, districtRestro);
 
-export default restroRouter ;
+export default restroRouter;
