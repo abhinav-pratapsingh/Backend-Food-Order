5fb325452b35096ff7ba4b6c12a55b03581f5423
Aniket Kumar <aniketpundir348@gmail.com>
2025-02-03 09:01:54 +0530

    sovke merge conflict

--- Backend/routes/foodRoutes.js
+++ Backend/routes/foodRoutes.js
@@ -1,15 +1,20 @@
-import express from 'express';
-import multer from 'multer';
-import { addfood, listFood, removeFood } from '../controller/foodController.js';
-import { authMiddlewareRestro } from '../middleware/auth.js';
-import trimValues from '../middleware/trimValues.js';
-import upload_middleware from '../config/multer.js';
-
+import express from "express";
+import multer from "multer";
+import { addfood, listFood, removeFood } from "../controller/foodController.js";
+import { authMiddlewareRestro } from "../middleware/auth.js";
+import trimValues from "../middleware/trimValues.js";
+import upload_middleware from "../config/multer.js";
 
 const foodRouter = express.Router();
 
-foodRouter.post('/add',trimValues,authMiddlewareRestro, upload_middleware, addfood);
-foodRouter.get('/list', authMiddlewareRestro, listFood);
-foodRouter.post('/remove',trimValues,authMiddlewareRestro, removeFood);
+foodRouter.post(
+  "/add",
+  trimValues,
+  authMiddlewareRestro,
+  upload_middleware,
+  addfood
+);
+foodRouter.post("/list", authMiddlewareRestro, listFood);
+foodRouter.post("/remove", trimValues, authMiddlewareRestro, removeFood);
 
 export default foodRouter;
