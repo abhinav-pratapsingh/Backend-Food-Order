41895874be056208988606db7e9b44f71f8678e2
Aniket Kumar <aniketpundir348@gmail.com>
2025-05-11 20:57:15 +0530

    add authMiddlewareRestro in orderRoutes

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -1,22 +1,50 @@
-import express from 'express';
-import {placeOrder,updateOrderStatus,userOrders, verifyPayment, getRazorpayOrders, listOrdersPending, listOrdersOutForDelivery, listOrdersDelivered} from "../controller/orderController.js";
-import { authMiddleware} from '../middleware/auth.js';
-import trimValues from '../middleware/trimValues.js';
+import express from "express";
+import {
+  placeOrder,
+  updateOrderStatus,
+  userOrders,
+  verifyPayment,
+  getRazorpayOrders,
+  listOrdersPending,
+  listOrdersOutForDelivery,
+  listOrdersDelivered,
+} from "../controller/orderController.js";
+import { authMiddleware, authMiddlewareRestro } from "../middleware/auth.js";
+import trimValues from "../middleware/trimValues.js";
 
 const orderRouter = express.Router();
 
 // user order routes
-orderRouter.post('/new',trimValues,authMiddleware,placeOrder);
-orderRouter.post('/userorders',trimValues,authMiddleware,userOrders);
-orderRouter.post('/verify',authMiddleware,verifyPayment);
-orderRouter.get('/getorder',getRazorpayOrders);
+orderRouter.post("/new", trimValues, authMiddleware, placeOrder);
+orderRouter.post("/userorders", trimValues, authMiddleware, userOrders);
+orderRouter.post("/verify", authMiddleware, verifyPayment);
+orderRouter.get("/getorder", getRazorpayOrders);
 
 // restro order routes
 
-orderRouter.get('/pending',trimValues,authMiddleware,listOrdersPending);
-orderRouter.get('/outfordelivery',trimValues,authMiddleware,listOrdersOutForDelivery);
-orderRouter.get('/delivered',trimValues,authMiddleware,listOrdersDelivered);
-orderRouter.post('/update',trimValues,updateOrderStatus);
-
+orderRouter.get(
+  "/pending",
+  trimValues,
+  authMiddlewareRestro,
+  listOrdersPending
+);
+orderRouter.get(
+  "/outfordelivery",
+  trimValues,
+  authMiddlewareRestro,
+  listOrdersOutForDelivery
+);
+orderRouter.get(
+  "/delivered",
+  trimValues,
+  authMiddlewareRestro,
+  listOrdersDelivered
+);
+orderRouter.post(
+  "/update",
+  trimValues,
+  authMiddlewareRestro,
+  updateOrderStatus
+);
 
 export default orderRouter;
