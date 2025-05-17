3191f08846ae6e761e908b34d1dc8a5f86ad3ba9
abhinav-pratapsingh <160205139+abhinav-pratapsingh@users.noreply.github.com>
2025-05-17 19:37:44 +0530

    Update restroRoutes.js
    add all routes

--- Backend/routes/restroRoutes.js
+++ Backend/routes/restroRoutes.js
@@ -5,7 +5,7 @@ import {
   restroRegister,
 } from "../controller/restroController.js";
 import { authMiddleware, checkRestroStatus } from "../middleware/auth.js";
-import { nearRestro } from "../controller/mapController.js";
+import { allRestrosForTest, nearRestro } from "../controller/mapController.js";
 import trimValues from "../middleware/trimValues.js";
 import upload_middleware from "../config/multer.js";
 import multer from "multer";
@@ -15,6 +15,7 @@ const restroRouter = express.Router();
 restroRouter.post("/register", upload_middleware, trimValues, restroRegister);
 restroRouter.post("/login", trimValues, checkRestroStatus, restroLogin);
 restroRouter.post("/near", multer().none(), trimValues, nearRestro);
+restroRouter.get("/all",allRestrosForTest);
 restroRouter.post("/district", trimValues, districtRestro);
 
 export default restroRouter;
